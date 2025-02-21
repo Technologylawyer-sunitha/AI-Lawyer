c04aa8a7b83ac4e23fbb253cb7623ba75eac0c7f
Technologylawyer-sunitha <sunithaks.acs@gmail.com>
2025-02-21 21:33:28 +0530

    Your commit message here

--- letsdo.py
+++ letsdo.py
@@ -1,3 +1,14 @@
 import streamlit as st
 
-st.write ("Hello")
+st.title("AI-Lawyer")
+st.write ("AI-Lawyer is a platform to draft, review and manage contracts, as well as handle legal & finance compliances with ease")
+
+st.sidebar.write("Finance")
+st.sidebar.write("Legal")
+st.file_uploader("Upload a file", type=["pdf", "docx"])
+
+st.write("OR")
+
+st.text_area("Paste your full contract or clause here", height=200)
+
+st.button("Submit")
